f23cc7a08f9480c0c7bc91d44c3de250f77c65dd
dgponce <dg38828@gmail.com>
2022-12-07 17:50:03 -0600

    Added quote comments and list routing

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -10,9 +10,9 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
   const { quoteId } = params;
-
   const quote = DUMMY_QUOTES.find(quote => quote.id === quoteId);
 
   if(!quote) {
@@ -22,7 +22,12 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`/quotes/${quoteId}/comments`}>
+      <Route path={`${match.path}`} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
