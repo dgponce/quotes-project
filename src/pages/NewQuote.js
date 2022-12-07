328a86e23ae8decef6948f88a3f3efebc4a5906e
dgponce <dg38828@gmail.com>
2022-12-07 17:21:02 -0600

    Added sorting quotes logic

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,16 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = quoteData => {
     const {author, text} = quoteData;
     console.log(author, text);
-  }
+
+    history.push('/quotes');
+  };
+
   return <QuoteForm onAddQuote={addQuoteHandler}/>
 };
 
